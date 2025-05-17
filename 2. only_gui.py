9e21d82199c6212059e794056ded5fd42b85ad1a
naganandana <naganandana17@gmail.com>
2025-05-17 10:45:31 +0400

    changed fan from PWM to on/off

--- 2. only_gui.py
+++ 2. only_gui.py
@@ -44,7 +44,7 @@ class ThariBakhoorApp(tk.Tk):
 
 
             # self.hx = HX711(dout_pin=18, pd_sck_pin=15)
-            self.hx = HX711(dout_pin=5, pd_sck_pin=6)
+            self.hx = HX711(5, 6)
             self.initialize_weight()
 
             self.sensor = self.pi.spi_open(0, 1000000, 0)
